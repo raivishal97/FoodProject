472b5a625f61ea0712ab2137a0c57a16270f23c3
Srivatsan <'vatsank@gmail.com'>
2019-11-06 12:49:34 +0530

    My First Commit

---
+++ QuickStart/src/com/training/Application.java
@@ -0,0 +1,15 @@
+package com.training;
+
+import java.util.*;
+
+public class Application {
+
+	public static void main(String[] args) {
+	
+		List<String> nameList = Arrays.asList("Ramesh","Suresh","Magesh");
+
+		nameList.forEach(System.out::println);
+		
+	}
+
+}
